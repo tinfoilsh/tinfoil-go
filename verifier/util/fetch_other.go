c9a170ea2ad312f8bcbfe2d24993a9ddeb5728a2
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-06-04 11:52:53 -0700

    fix: always close HTTP response bodies on failed requests

--- verifier/util/fetch_other.go
+++ verifier/util/fetch_other.go
@@ -12,10 +12,10 @@ func Get(url string) ([]byte, map[string][]string, error) {
 	if err != nil {
 		return nil, nil, err
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode > 299 {
 		return nil, nil, fmt.Errorf("HTTP GET %s: %d %s", url, resp.StatusCode, resp.Status)
 	}
-	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, nil, err
@@ -28,10 +28,10 @@ func Post(url, contentType string, reqBody []byte) ([]byte, map[string][]string,
 	if err != nil {
 		return nil, nil, err
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode > 299 {
 		return nil, nil, fmt.Errorf("HTTP POST %s: %d %s", url, resp.StatusCode, resp.Status)
 	}
-	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, nil, err
